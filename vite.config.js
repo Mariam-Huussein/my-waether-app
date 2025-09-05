63a95b86e7173a6e4497031ea5fc97e6c5996115
Mariam-Huussein <mariamhusseen4@gmail.com>
2025-09-06 01:38:12 +0300

    Fix Error 404

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://mariam-huussein.github.io/my-waether-app/',
+  base: '/my-waether-app/',
   plugins: [react()],
 })
